2c37aa7447e6a556ec4be204e0c623a4bdc0ffc4
Thomas Edson <andepipous@gmail.com>
2024-08-19 15:52:16 -0400

    feat: add transations header

--- src/app/pages/tabs/transactions/transactions.page.ts
+++ src/app/pages/tabs/transactions/transactions.page.ts
@@ -1,16 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { transactionsData } from 'src/app/data/Transactions.data';
+import { Transactions } from 'src/app/models/home-models/Transactions.model';
 
 @Component({
   selector: 'app-transactions',
   templateUrl: './transactions.page.html',
   styleUrls: ['./transactions.page.scss'],
 })
-export class TransactionsPage {
-  allTransactions: any[] = [];
-  transactions: any[] = [];
+export class TransactionsPage implements OnInit {
+  allTransactions: Transactions[] = [];
+  filteredTransactions: any[] = [];
   segmentValue = 'in';
 
   constructor() {}
 
-  segmentChanged(ev: any) {}
+  ngOnInit() {
+    this.allTransactions = transactionsData;
+    this.filterTransactions();
+  }
+
+  filterTransactions() {
+    if (this.segmentValue === 'in') {
+      this.filteredTransactions = this.allTransactions.filter(
+        (item) => item.amount >= 0
+      );
+    } else {
+      this.allTransactions.filter((item) => item.amount <= 0);
+    }
+  }
+  segmentChanged(ev: CustomEvent) {
+    this.segmentValue = ev.detail.value;
+    this.filterTransactions();
+  }
 }
