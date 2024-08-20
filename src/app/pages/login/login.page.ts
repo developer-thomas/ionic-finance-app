03e24f980d75ceec5b1ff336aa016c6be897c187
Thomas Edson <andepipous@gmail.com>
2024-08-20 14:24:52 -0400

    feat: created login page

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+type InputType = 'password' | 'text';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  myForm!: FormGroup;
+  isVisible: InputType = 'password';
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      cpf: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(11),
+          Validators.maxLength(11),
+        ],
+      ],
+      senha: ['', [Validators.required, Validators.minLength(8)]],
+    });
   }
 
+  changeInputPasswordType() {
+    this.isVisible = this.isVisible === 'password' ? 'text' : 'password';
+  }
+
+  onSubmit() {
+    console.log(this.myForm.value);
+    this.myForm.reset();
+  }
 }
