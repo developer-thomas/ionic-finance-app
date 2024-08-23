dd7a68d4bda735716b2151404bb43f89bd78ca12
Thomas Edson <andepipous@gmail.com>
2024-08-22 22:45:21 -0400

    feat: create login service to comunicate with api

--- src/app/services/users/login.service.ts
+++ src/app/services/users/login.service.ts
@@ -1,20 +1,38 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
+
+interface userCredentials {
+  cpf: string;
+  passwsord: string;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class LoginService {
   constructor(private http: HttpClient) {}
 
-  userCredentials(cpf: string, password: string): Observable<any> {
-    return this.http.post<{ cpf: string; password: string }>(
-      `${environment.base_url}/login`,
-      {
-        cpf,
-        password,
-      }
-    );
+  userCredentials(credentials: {
+    cpf: string;
+    password: string;
+  }): Observable<any> {
+    return this.http
+      .post<userCredentials>(`${environment.base_url}/login`, credentials)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(responseError: HttpErrorResponse) {
+    let errorMessage = '';
+
+    if (responseError.error instanceof ErrorEvent) {
+      // Erro do lado do cliente
+      errorMessage = responseError.error.message;
+    } else {
+      errorMessage = responseError.error.message;
+    }
+
+    return throwError(() => new Error(errorMessage));
   }
 }
