99b0e48017a9e1bca8c1ac3cb9ee34f6d78b232d
Thomas Edson <andepipous@gmail.com>
2024-08-22 22:46:05 -0400

    feat: create login post method, spinners and toats to treat the request and response api

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LoadingController, ToastController } from '@ionic/angular';
+import { BehaviorSubject } from 'rxjs';
 import { LoginService } from 'src/app/services/users/login.service';
 
 type InputType = 'password' | 'text';
@@ -17,7 +19,9 @@ export class LoginPage {
   constructor(
     private fb: FormBuilder,
     private router: Router,
-    private loginService: LoginService
+    private loginService: LoginService,
+    private toastController: ToastController,
+    private loadingController: LoadingController
   ) {
     this.myForm = this.fb.group({
       cpf: [
@@ -37,16 +41,48 @@ export class LoginPage {
     this.isVisible = this.isVisible === 'password' ? 'text' : 'password';
   }
 
-  onSubmit() {
-    // this.router.navigateByUrl('/tabs/home');
-    this.loginService
-      .userCredentials(
-        this.myForm.get('cpf')?.value,
-        this.myForm.get('password')?.value
-      )
-      .subscribe((res) => {
-        console.log(res);
-      });
-    this.myForm.reset();
+  async onSubmit() {
+    let credentials = {
+      cpf: this.myForm.get('cpf')?.value,
+      password: this.myForm.get('password')?.value,
+    };
+
+    await this.loginSpinner();
+
+    this.loginService.userCredentials(credentials).subscribe({
+      next: async (response) => {
+        // usar o jwt no back e pegar o token aqui
+
+        this.loadingController.dismiss();
+      },
+      error: async (error) => {
+        await this.loadingController.dismiss();
+        await this.loginErrorToast();
+      },
+      complete: () => {
+        console.log('completed');
+        this.router.navigateByUrl('/tabs/home');
+        this.myForm.reset();
+      },
+    });
+  }
+
+  async loginErrorToast() {
+    const toast = await this.toastController.create({
+      message: 'CPF ou senha incorretos.',
+      duration: 5000,
+      position: 'bottom',
+      color: 'danger',
+    });
+    toast.present();
+  }
+
+  async loginSpinner() {
+    const spinner = await this.loadingController.create({
+      spinner: 'bubbles',
+      message: 'Aguarde...',
+    });
+
+    spinner.present();
   }
 }
