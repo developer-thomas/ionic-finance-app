4e0909f4babb7506974191a0d209409c0292c462
Thomas Edson <andepipous@gmail.com>
2024-08-23 23:35:25 -0400

    feat: add auth guard to tabs route and its children

--- src/app/pages/tabs/tabs-routing.module.ts
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -2,34 +2,36 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { TabsPage } from './tabs.page';
+import { AuthGuard } from 'src/app/services/users/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
+    canActivate: [AuthGuard],
     component: TabsPage,
     children: [
       {
-        path: 'home',
+        path: 'home/:id',
         pathMatch: 'full',
         loadChildren: () =>
           import('./home/home.module').then((m) => m.HomePageModule),
       },
       {
-        path: 'transactions',
+        path: 'transactions/:id',
         loadChildren: () =>
           import('./transactions/transactions.module').then(
             (m) => m.TransactionsPageModule
           ),
       },
       {
-        path: 'settings',
+        path: 'settings/:id',
         loadChildren: () =>
           import('./settings/settings.module').then(
             (m) => m.SettingsPageModule
           ),
       },
       {
-        path: 'card',
+        path: 'card/:id',
         loadChildren: () =>
           import('./card/card.module').then((m) => m.CardPageModule),
       },
